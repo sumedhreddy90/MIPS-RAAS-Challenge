bf7ba47f91be1f145dfe2ee1d2bf8e25201098f0
sumedhreddy90 <sumedhreddykoppula@gmail.com>
2022-08-08 23:57:52 -0400

    Add usb_camera drivers and modify aruco wrapper

--- ros2_aruco/launch/usb_camera_node.launch.py
+++ ros2_aruco/launch/usb_camera_node.launch.py
@@ -13,17 +13,17 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='usb_camera_driver',
-            node_executable='usb_camera_driver_node',
+            executable='usb_camera_driver_node',
             output="screen",
-            node_namespace=ns,
-            parameters=[{"camera_calibration_file": "file:///home/spragunr/.ros/camera_info/camera.yaml"}],
+            namespace=ns,
+            parameters=[{"camera_calibration_file": "file:///home/sumedh/.ros/camera_info/camera.yaml"}],
             remappings=[
                 ('/camera/camera_info', '/camera/camera_info'),
                 ('/camera/image', '/camera/image_raw')]
         ),
         Node(
             package="tf2_ros",
-            node_executable="static_transform_publisher",
+            executable="static_transform_publisher",
             arguments=["0","0","0","0","0","0","world","camera"],
             output="screen")
 
